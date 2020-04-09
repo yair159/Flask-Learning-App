184d7baf967bd5d2122d23aad1ae782ef9a852b5
Yair Etzion <yetzion@outbrain.com>
2020-04-09 18:50:46 +0300

    mt first commit

---
+++ flask_blog/routes.py
@@ -0,0 +1,68 @@
+from flask import render_template, url_for, flash, redirect, request
+from flask_blog import app, db, bcrypt
+from flask_blog.forms import RegistrationForm, LoginForm
+from flask_blog.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
+
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+@app.route('/')
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', posts=posts)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hased_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hased_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your accont has been created!!', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password , form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else (url_for('home'))
+        else:
+            flash('failed to log in', 'danger')    
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+    
+
+@app.route('/account')
+@login_required
+def account():
+    return render_template('account.html', title='Account')
