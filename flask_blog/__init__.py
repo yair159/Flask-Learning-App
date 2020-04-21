d79b70983c726484904329be3af0d172352a27f5
Yair Etzion <yetzion@outbrain.com>
2020-04-21 14:05:25 +0300

    update to lesson 11

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -2,14 +2,32 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+from flask_blog.config import Confing
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login' #the login_manager knows where to direct us when a route need a login
 login_manager.login_message_category = 'info'
+mail = Mail()
 
-from flask_blog import routes
+def create_app(config_class=Confing):
+    app = Flask(__name__)
+    app.config.from_object(Confing)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    # from flask_blog import routes
+    from flask_blog.users.routes import users
+    from flask_blog.posts.routes import posts
+    from flask_blog.main.routes import main
+
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
